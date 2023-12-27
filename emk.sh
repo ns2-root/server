6a74cac45b60dfab19401f1a3abb08aaa4428c40
enes2 <ekaya2110@posta.pau.edu.tr>
2023-12-27 23:52:11 +0300

    malware

--- emk.sh
+++ emk.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-dns() {
-    service=$1
-    iprecords=$(nslookup $service | grep -A1 "Name:" | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
-    
+service=$1
+    iprecords=$(nslookup $service | grep -A1 "Name:" | grep -oE '\b([0-9]{1,3}\.){3}[0-9]$
     if [ -z "$iprecords" ]; then
         echo "Error: Unable to resolve DNS for $service"
     else
         echo $iprecords
     fi
-}
